f9048ca96c21ff56290b5d0b24dd76301f046a1d
Amit Garu <amitgaru2@gmail.com>
2024-02-08 12:00:06 -0700

    bs to ad and ad to bs api

--- project/application/main.py
+++ project/application/main.py
@@ -1,8 +1,10 @@
+import datetime
 import nepali_datetime
 
 from pydantic import BaseModel
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title="Nepali Datetime",
@@ -28,14 +30,24 @@ def nepali_datetime_date(fmt: str = Query('%Y-%m-%d', alias='format')):
     return {"data": data.strftime(fmt)}
 
 
-# @app.get("/date/convert/bs-ad", response_model=DateResponse)
-# def nepali_datetime_date_convert_bs_ad(bs_date: str = Query('%Y-%m-%d', alias='format')):
-#     return {}
+@app.get("/date/convert/bs-ad", response_model=DateResponse)
+def nepali_datetime_date_convert_bs_ad(bs_date: str = Query('2080-10-29', alias='BS Date'), fmt: str = Query("%Y-%m-%d", alias="format")):
+    try:
+        bs_date = nepali_datetime.datetime.strptime(bs_date, "%Y-%m-%d").date()
+        ad_date = nepali_datetime.date.to_datetime_date(bs_date).strftime(fmt)
+    except Exception:
+        raise HTTPException(status_code=400, detail="Something wrong with your input!")
+    return {"data": ad_date}
 
 
-# @app.get("/date/convert/ad-bs", response_model=DateResponse)
-# def nepali_datetime_date_convert_ad_bs(ad_date: str = Query('%Y-%m-%d', alias='format')):
-#     return {}
+@app.get("/date/convert/ad-bs", response_model=DateResponse)
+def nepali_datetime_date_convert_ad_bs(ad_date: str = Query('2024-02-12', alias='AD Date'), fmt: str = Query("%Y-%m-%d", alias="format")):
+    try:
+        ad_date = datetime.datetime.strptime(ad_date, "%Y-%m-%d").date()
+        bs_date = nepali_datetime.date.from_datetime_date(ad_date).strftime(fmt)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Something wrong with your input!")
+    return {"data": bs_date}
 
 
 @app.get("/datetime", response_model=DatetimeResponse)
